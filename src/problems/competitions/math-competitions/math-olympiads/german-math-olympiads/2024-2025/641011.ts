d4af34771f822fa90111ae481a191d83e5f5f2d9
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2024-09-06 14:37:20 +0100

    Add solution to part b

--- src/problems/competitions/math-competitions/math-olympiads/german-math-olympiads/2024-2025/641011.ts
+++ src/problems/competitions/math-competitions/math-olympiads/german-math-olympiads/2024-2025/641011.ts
@@ -12,3 +12,18 @@ for (let x = -2; x <= 2; x++) {
 
 console.log("Es gibt " + solutions.length + " Lösungen.");
 console.log(solutions);
+
+solutions.length = 0;
+
+for (let x = -3; x <= 3; x++) {
+    for (let y = -3; y <= 3; y++) {
+        for (let z = -3; z <= 3; z++) {
+            if (x * x + y * y + z * z === 9) {
+                solutions.push([x, y, z]);
+            }
+        }
+    }
+}
+
+console.log("Es gibt " + solutions.length + " Lösungen.");
+console.log(solutions);
